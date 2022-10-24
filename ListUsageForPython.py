784d026b03861220422c504257fe10d5e4d9fd18
jhoffman222 <57236159+jhoffman222@users.noreply.github.com>
2022-10-24 11:18:11 -0400

    show how to use lists data structure in python

---
+++ ListUsageForPython.py
@@ -0,0 +1,14 @@
+List = []
+print("This is what a blank list looks like!:")
+print(List)
+
+List = [8, 7, 55, 2, 45, 87]
+print("\nSome numbers:")
+print(List)
+
+List = ["Programming", "is", "fun"]
+print("\nThe words:")
+print(List[0]) 
+print(List[2])
+
+print("Now we know how to work with lists in Python!")
