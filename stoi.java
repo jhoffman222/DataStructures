4fd77d311da1ba61a6630d38e4ab1398abf71369
Cgaro21 <93489330+Cgaro21@users.noreply.github.com>
2022-10-31 06:54:51 -0400

    Create string to integer converter in java

---
+++ stoi.java
@@ -0,0 +1,33 @@
+public int atoi(String str) {
+	if (str == null || str.length() < 1)
+		return 0;
+ 
+	str = str.trim();
+ 
+	char flag = '+';
+  
+	int i = 0;
+	if (str.charAt(0) == '-') {
+		flag = '-';
+		i++;
+	} else if (str.charAt(0) == '+') {
+		i++;
+	}
+	double result = 0;
+ 
+	while (str.length() > i && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+		result = result * 10 + (str.charAt(i) - '0');
+		i++;
+	}
+ 
+	if (flag == '-')
+		result = -result;
+ 
+	if (result > Integer.MAX_VALUE)
+		return Integer.MAX_VALUE;
+ 
+	if (result < Integer.MIN_VALUE)
+		return Integer.MIN_VALUE;
+ 
+	return (int) result;
+}
